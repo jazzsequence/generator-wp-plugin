990d876801931e1f208a98b4af08c6f147ad4390
Chris Reynolds <chris@jazzsequence.com>
2016-12-06 11:11:19 -0700

    adds namespace.php

---
+++ app/templates/namespace.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * <% name %>
+ *
+ * @package <% project %>\<% namespace %>
+ */
+
+namespace < % project % > \ < % namespace % > ;
+
+/**
+ * Hook all the things.
+ *
+ * @since  <% version %>
+ */
+function bootstrap() {
+	// Add all your plugin hooks here.
+}
