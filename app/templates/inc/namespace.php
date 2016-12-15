7b8616ad60a613be5bb089677e34c415c63ca47a
Ryan McCue <me@ryanmccue.info>
2016-12-15 15:48:59 +1000

    Add "basic" autoloader option

--- app/templates/inc/namespace.php
+++ app/templates/inc/namespace.php
@@ -10,6 +10,30 @@ namespace <%= project %>\<%= namespace %>;
  *
  * Registers actions and filter required to run the plugin.
  */
-function bootstrap() {
+function bootstrap() {<%
+if ( autoloader == 'basic' ) { %>
+	spl_autoload_register( __NAMESPACE__ . '\\autoload' );
+<% }
+%>
 	// Add all your plugin hooks here.
-}
+}<% if ( autoloader == 'basic' ) { %>
+
+/**
+ * Autoload classes for this namespace.
+ *
+ * @param string $class Class name.
+ */
+function autoload( $class ) {
+	if ( strpos( $class, __NAMESPACE__ . '\\' ) !== 0 ) {
+		return;
+	}
+
+	$relative = strtolower( substr( $class, strlen( __NAMESPACE__ . '\\' ) ) );
+	$parts = explode( '\\', $relative );
+	$final = array_pop( $parts );
+	array_push( $parts, 'class-' . $final . '.php' );
+	$path = __DIR__ . '/' . implode( '/', $parts );
+
+	require $path;
+}<% }
+%>
